40389c720ca9bd12c033a0c01207f4b4265ea2b9
love-somnus <928200207@qq.com>
2016-02-01 18:12:26 +0800

    version1.1.1

--- src/main/webapp/form/timefield.js
+++ src/main/webapp/form/timefield.js
@@ -10,7 +10,7 @@ Ext.onReady(function(){
 		items:[{
 			fieldLabel:'时间选择框',
 			xtype : 'timefield',
-			width : 220,
+			width : 300,
 			labelSeparator :'：',//分隔符
 			msgTarget :'side',//在字段的右边显示一个提示信息
 			autoFitErrors : false,//展示错误信息时是否自动调整字段组件宽度
@@ -18,7 +18,7 @@ Ext.onReady(function(){
 			maxText : '时间应小于{0}',//大于最大时间的提示信息
 			minValue : '10:00',//最小时间
 			minText : '时间应大于{0}',//小于最小时间的提示信息
-			pickerMaxHeight : 70,//下拉列表的最大高度
+			pickerMaxHeight : 150,//下拉列表的最大高度
 			increment : 60,//时间间隔为60分钟
 			format : 'G时i分s秒 ',//G标示为24时计时法
 			invalidText :'时间格式无效'
